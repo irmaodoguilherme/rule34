6d51e1c9e1db5fc73f062301c751538b65192793
irmaodoguilherme <airesguilherme83@gmail.com>
2024-11-21 00:26:40 -0300

    testing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js';
-import { getFirestore, collection, addDoc, getDoc, getDocs, doc, query, where } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
+import { getFirestore, collection, addDoc, getDoc, getDocs, doc, query, where, serverTimestamp } from 'https://www.gstatic.com/firebasejs/11.0.2/firebase-firestore.js'
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-auth.js"
 
 const ulMedia = document.querySelector('[data-js="media-list"]')
 const mediaPopup = document.querySelector('[data-js="media-popup"]')
@@ -9,7 +10,15 @@ const buttonDownload = document.querySelector('[data-js="download-media"]')
 const buttonLikeMedia = document.querySelector('[data-js="like-media"]')
 const buttonBookmarkMedia = document.querySelector('[data-js="bookmark-media"]')
 const buttonProfile = document.querySelector('[data-button="profile"]')
+const buttonLogin = document.querySelector('[data-button="login"]')
+const buttonLogout = document.querySelector('[data-button="logout"]')
 const buttonHome = document.querySelector('[data-button="home"]')
+const buttonShowLikes = document.querySelector('[data-button="show-likes"]')
+const buttonShowBookmarks = document.querySelector('[data-button="show-bookmarks"]')
+const offcanvas = document.querySelector('[data-js="offcanvas"]')
+const offcanvasPopup = document.querySelector('[data-js="offcanvas-popup"]')
+const buttonShowActivity = document.querySelector('[data-button="show-activity"]')
+const mediaButtons = document.querySelector('[data-js="media-buttons"]')
 
 const firebaseConfig = {
   apiKey: "AIzaSyC3ibhZluc8MPpS0JtFhhnzsfcdz-0W9a4",
@@ -22,6 +31,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
 const collectionMedia = collection(db, 'media')
+const provider = new GoogleAuthProvider()
+const auth = getAuth()
 
 formFetchMedia.addEventListener('submit', async e => {
   e.preventDefault()
@@ -44,7 +55,7 @@ formFetchMedia.addEventListener('submit', async e => {
     mediaImg.setAttribute('data-Tags', media.tags)
     mediaImg.setAttribute('data-image', media.image)
 
-    if(media.image.includes('mp4')) {
+    if (media.image.includes('mp4')) {
       mediaImg.classList.add('video-outline')
     }
 
@@ -56,7 +67,7 @@ formFetchMedia.addEventListener('submit', async e => {
   ulMedia.append(documentFragment)
 })
 
-ulMedia.addEventListener('click', e => {
+ulMedia.addEventListener('click', async e => {
   const dataMedia = e.target.dataset
 
   if (!dataMedia.fileurl) {
@@ -65,6 +76,25 @@ ulMedia.addEventListener('click', e => {
 
   const [mediaPreviewUrl, mediaFileUrl, mediaId, mediaOwner, mediaTags, mediaImage] =
     [e.target.src, dataMedia.fileurl, dataMedia.id, dataMedia.owner, dataMedia.tags, dataMedia.image]
+
+  // const isMediaLiked = (await getDocs(query(collectionMedia, where('id', '==', mediaId), where('like', '!=', 'false')))).docs.length
+  // const isMediaBookmarked = (await getDocs(query(collectionMedia, where('id', '==', mediaId), where('bookmark', '!=', 'false')))).docs.length
+
+  // if (isMediaLiked) {
+  //   buttonLikeMedia.classList.remove('bi-heart')
+  //   buttonLikeMedia.classList.add('bi-heart-fill')
+  // } else {
+  //   buttonLikeMedia.classList.remove('bi-heart-fill')
+  //   buttonLikeMedia.classList.add('bi-heart')
+  // }
+
+  // if (isMediaBookmarked) {
+  //   buttonBookmarkMedia.classList.remove('bi-bookmark')
+  //   buttonBookmarkMedia.classList.add('bi-bookmark-fill')
+  // } else {
+  //   buttonBookmarkMedia.classList.remove('bi-bookmark-fill')
+  //   buttonBookmarkMedia.classList.add('bi-bookmark')
+  // }
 
   if (mediaImage.includes('.mp4')) {
     const mediaVideo = document.createElement('video')
@@ -110,17 +140,7 @@ mediaPopup.addEventListener('click', e => {
   }
 })
 
-// buttonClosePopup.addEventListener('click', () => {
-//   const media = document.querySelector('[data-js="media"]')
-
-//   mediaPopup.classList.add('hide')
-
-//   if (media) {
-//     media.remove()
-//   }
-// })
-
-buttonLikeMedia.addEventListener('click', async () => {
+const likeMedia = async userid => {
   const media = document.querySelector('[data-js="media"]')
   const dataMedia = media.dataset
   const [mediaFileUrl, mediaPreviewUrl, mediaId, mediaOwner, mediaTags, mediaImage] =
@@ -135,27 +155,76 @@ buttonLikeMedia.addEventListener('click', async () => {
     id: mediaId,
     owner: mediaOwner,
     tags: mediaTags,
-    image: mediaImage
+    image: mediaImage,
+    time: serverTimestamp(),
+    like: true,
+    bookmark: false,
+    userid
   })
-})
+}
 
-buttonBookmarkMedia.addEventListener('click', () => {
+const bookmarkMedia = async userid => {
+  const media = document.querySelector('[data-js="media"]')
+  const dataMedia = media.dataset
+  const [mediaFileUrl, mediaPreviewUrl, mediaId, mediaOwner, mediaTags, mediaImage] =
+    [media.src, dataMedia.previewurl, dataMedia.id, dataMedia.owner, dataMedia.tags, dataMedia.image]
+
   buttonBookmarkMedia.classList.toggle('bi-bookmark')
   buttonBookmarkMedia.classList.toggle('bi-bookmark-fill')
+
+  await addDoc(collectionMedia, {
+    fileUrl: mediaFileUrl,
+    previewurl: mediaPreviewUrl,
+    id: mediaId,
+    owner: mediaOwner,
+    tags: mediaTags,
+    image: mediaImage,
+    time: serverTimestamp(),
+    like: false,
+    bookmark: true,
+    userid
+  })
+}
+
+buttonLogin.addEventListener('click', () => signInWithPopup(auth, provider))
+
+buttonHome.addEventListener('click', async () => {
+  ulMedia.innerHTML = ''
 })
 
-buttonProfile.addEventListener('click', async () => {
+buttonProfile.addEventListener('click', () => {
+  offcanvasPopup.classList.remove('hide')
+  setTimeout(() => offcanvas.classList.add('show-offcanvas'), 100)
+})
+
+offcanvasPopup.addEventListener('click', e => {
+  const clickedElement = e.target
+  const dataClickedElement = clickedElement.dataset.js
+
+  if (dataClickedElement != 'offcanvas') {
+    offcanvas.classList.remove('show-offcanvas')
+    setTimeout(() => offcanvasPopup.classList.add('hide'), 250)
+  }
+})
+
+buttonLogout.addEventListener('click', () => {
+  signOut(auth)
+  offcanvas.classList.remove('show-offcanvas')
+  setTimeout(() => offcanvasPopup.classList.add('hide'), 250)
+})
+
+buttonShowLikes.addEventListener('click', async () => {
+  offcanvas.classList.remove('show-offcanvas')
+  setTimeout(() => offcanvasPopup.classList.add('hide'), 250)
   ulMedia.innerHTML = ''
-
-  const x = await getDocs(query(collectionMedia, where('image', '!=', false)))
+  const likedMedia = await getDocs(query(collectionMedia, where('like', '!=', false)))
   const documentFragment = new DocumentFragment()
+  likedMedia.docs.forEach(doc => {
+    const { fileUrl, id, owner, previewurl, tags, image, time, like, bookmark } = doc.data()
 
-  x.docs.forEach(doc => {
-    const { fileUrl, id, owner, previewurl, tags, image } = doc.data()
-    
     const mediaLi = document.createElement('li')
     mediaLi.classList.add('custom')
-  
+
     const mediaImg = document.createElement('img')
     mediaImg.src = previewurl
     mediaImg.classList.add('clickable')
@@ -164,24 +233,101 @@ buttonProfile.addEventListener('click', async () => {
     mediaImg.setAttribute('data-Owner', owner)
     mediaImg.setAttribute('data-Tags', tags)
     mediaImg.setAttribute('data-image', image)
+    mediaImg.setAttribute('data-time', time)
+    mediaImg.setAttribute('data-like', like)
+    mediaImg.setAttribute('data-bookmark', bookmark)
 
-    if(image.includes('.mp4')) {
+    if (image.includes('.mp4')) {
       mediaImg.classList.add('video-outline')
     }
-  
+
     mediaLi.append(mediaImg)
     documentFragment.append(mediaLi)
   })
 
-  // const docData = (await getDoc(doc(db, "media", 'iIVyPRPj1x7CcUrLTbdS'))).data()
+  ulMedia.append(documentFragment)
+})
+
+buttonShowActivity.addEventListener('click', async () => {
+  ulMedia.innerHTML = ''
+  const activity = await getDocs(collectionMedia)
+  const documentFragment = new DocumentFragment()
+  activity.docs.forEach(doc => {
+    const { fileUrl, id, owner, previewurl, tags, image, time, like, bookmark } = doc.data()
+
+    const mediaLi = document.createElement('li')
+    mediaLi.classList.add('custom')
+
+    const mediaImg = document.createElement('img')
+    mediaImg.src = previewurl
+    mediaImg.classList.add('clickable')
+    mediaImg.setAttribute('data-fileUrl', fileUrl)
+    mediaImg.setAttribute('data-Id', id)
+    mediaImg.setAttribute('data-Owner', owner)
+    mediaImg.setAttribute('data-Tags', tags)
+    mediaImg.setAttribute('data-image', image)
+    mediaImg.setAttribute('data-time', time)
+    mediaImg.setAttribute('data-like', like)
+    mediaImg.setAttribute('data-bookmark', bookmark)
+
+    if (image.includes('.mp4')) {
+      mediaImg.classList.add('video-outline')
+    }
+
+    mediaLi.append(mediaImg)
+    documentFragment.append(mediaLi)
+  })
 
   ulMedia.append(documentFragment)
 })
 
-buttonHome.addEventListener('click', async () => {
+buttonShowBookmarks.addEventListener('click', async () => {
   ulMedia.innerHTML = ''
+  const bookmarks = await getDocs(query(collectionMedia, where('bookmark', '!=', false)))
+  const documentFragment = new DocumentFragment()
+  bookmarks.docs.forEach(doc => {
+    const { fileUrl, id, owner, previewurl, tags, image, time, like, bookmark } = doc.data()
+
+    const mediaLi = document.createElement('li')
+    mediaLi.classList.add('custom')
+
+    const mediaImg = document.createElement('img')
+    mediaImg.src = previewurl
+    mediaImg.classList.add('clickable')
+    mediaImg.setAttribute('data-fileUrl', fileUrl)
+    mediaImg.setAttribute('data-Id', id)
+    mediaImg.setAttribute('data-Owner', owner)
+    mediaImg.setAttribute('data-Tags', tags)
+    mediaImg.setAttribute('data-image', image)
+    mediaImg.setAttribute('data-time', time)
+    mediaImg.setAttribute('data-like', like)
+    mediaImg.setAttribute('data-bookmark', bookmark)
+
+    if (image.includes('.mp4')) {
+      mediaImg.classList.add('video-outline')
+    }
+
+    mediaLi.append(mediaImg)
+    documentFragment.append(mediaLi)
+  })
+
+  ulMedia.append(documentFragment)
 })
 
-/* https://api-cdn.rule34.xxx/images/2201/8c62233a4bd7215a1322f5e36d7e203c.png */
+onAuthStateChanged(auth, user => {
+  if (!user) {
+    buttonLogin.classList.remove('hide')
+    buttonProfile.classList.add('hide')
+    mediaButtons.classList.add('hide')
+    buttonLikeMedia.onclick = ''
+    buttonBookmarkMedia.onclick = ''
+    return
+  }
 
-let x;
+  buttonLogin.classList.add('hide')
+  buttonProfile.classList.remove('hide')
+  mediaButtons.classList.remove('hide')
+  buttonLikeMedia.onclick = () => likeMedia(user.uid)
+  buttonBookmarkMedia.onclick = () => bookmarkMedia(user.uid)
+})
+/* https://api-cdn.rule34.xxx/images/2201/8c62233a4bd7215a1322f5e36d7e203c.png */
